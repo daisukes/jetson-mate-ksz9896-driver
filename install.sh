fa1a46b637a3ba21a2fbfbd5cba00696dff79a98
Daisuke Sato <daisukes@cmu.edu>
2022-05-17 13:01:56 -0400

    (#1) update install.sh to check i2c bus 0 and 1 to detect address 0x5f chip

--- install.sh
+++ install.sh
@@ -4,5 +4,14 @@ pushd driver
 make
 sudo make install
 popd
-sudo cp ksz9897 /etc/init.d/
+
+for bus in 0 1; do
+  echo "checking i2c bus $bus"
+  detect=$(i2cdetect -y -a $bus 0x5f 0x5f)
+  if [ $(echo $detect | grep 5f | wc -l) -eq 1 ] || [ $(echo $detect | grep UU | wc -l) -eq 1 ]; then
+    echo "found a chip on 0x5f"
+    sed s/i2c-./i2c-$bus/ ksz9897 | sudo tee /etc/init.d/ksz9897
+    sudo chmod +x /etc/init.d/ksz9897
+  fi
+done
 sudo update-rc.d ksz9897 defaults
